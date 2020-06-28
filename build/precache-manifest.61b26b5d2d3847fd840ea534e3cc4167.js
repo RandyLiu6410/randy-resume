7d5f745b766c1f34127e06d278b23b8d72a0e0b8
RandyLiu6410 <wert6410@gmail.com>
2020-06-28 19:54:36 +0800

    Initial

---
+++ build/precache-manifest.61b26b5d2d3847fd840ea534e3cc4167.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d11cde8867ba3ae3dc8a4370e8fe134",
+    "url": "/index.html"
+  },
+  {
+    "revision": "803fb91b632d9c0f5c62",
+    "url": "/static/css/2.b3aa7b05.chunk.css"
+  },
+  {
+    "revision": "b2cad9c8e40483bdad7f",
+    "url": "/static/css/main.7a924db1.chunk.css"
+  },
+  {
+    "revision": "803fb91b632d9c0f5c62",
+    "url": "/static/js/2.75b9547c.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.75b9547c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2cad9c8e40483bdad7f",
+    "url": "/static/js/main.9a1a89a5.chunk.js"
+  },
+  {
+    "revision": "e75b71bc4122a5e2252f",
+    "url": "/static/js/runtime-main.472794ff.js"
+  }
+]);
